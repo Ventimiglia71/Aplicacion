e8d2454224de6d311181bb652336f342c75eb5b6
Ventimiglia71 <ifct0609albertoluis@gmail.com>
2020-12-18 10:30:21 +0100

    20201217Backup
    Cambios en el Menú y en BBDD, avance en reservas

---
+++ aulas/src/main/java/com/cice/aulas/repositories/IReservaRepository.java
@@ -0,0 +1,16 @@
+package com.cice.aulas.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cice.aulas.entities.Reserva;
+
+public interface IReservaRepository extends JpaRepository<Reserva, Integer> {
+	@Query(nativeQuery = true, value = "SELECT * FROM reservas WHERE ano = :anoSelec")
+	List<Reserva> existeAnyo(@Param("anoSelec") Integer anoSelec);
+	
+
+}
